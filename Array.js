90e0bd95dd35d4a88acd2e3cd60623b8d95652af
Mavo <rutvikpatel95862@gmail.com>
2023-07-04 15:36:56 +0530

    rutvik

--- Array.js
+++ Array.js
@@ -2,9 +2,13 @@ const a= [];
 a.push("jay","vishal","rutvik","rohan");
 console.log(a);
 a[7]="nil";
-console.log(a[3]);
-console.log(Object.keys(a));
-a.length=4;
+console.log(a[7]);
+console.log(a.length);
+
 console.log(Object.keys(a));
 console.log(a);
+
+ a.length=4;
+console.log(Object.keys(a));
+ console.log(a);
 
