8e2218e424fe13005e909ab1223046307fc3a858
Mavo <rutvikpatel95862@gmail.com>
2023-07-05 16:07:09 +0530

    rutvik

--- Array.js
+++ Array.js
@@ -1,14 +1,45 @@
-const a= [];
-a.push("jay","vishal","rutvik","rohan");
-console.log(a);
-a[7]="nil";
-console.log(a[7]);
-console.log(a.length);
+// const a= [];
+// a.push("jay","vishal","rutvik","rohan");
+// console.log(a);
+// a[7]="nil";
+// console.log(a[7]);
+// console.log(a.length);
 
-console.log(Object.keys(a));
-console.log(a);
+// console.log(Object.keys(a));
+// console.log(a);
 
- a.length=4;
-console.log(Object.keys(a));
- console.log(a);
+//  a.length=4;
+// console.log(Object.keys(a));
+//  console.log(a);
 
+// let a=["yuvaraj","kajal","rutvik","nil"];
+// console.log(a[2],a[3]);
+
+// Array length properties
+
+// let a=[
+//    "a",
+//    "b",
+//    "c",
+//    "d"
+// ];
+// let b=a[4]="e";
+// console.log(a);
+// console.log(b);
+
+/*accessing the first element*/
+// let a=["yuvaraj","kajal","rutvik","nil"];
+// console.log(a[1]);
+// console.log(a);
+
+/*accessing the last Element*/
+
+// let a=["yuvaraj","kajal","rutvik","nil"];
+// console.log(a.length-3);
+// console.log(a);
+
+//adding array Element
+{
+    let a=["yuvaraj","kajal","rutvik","nil"];
+
+}
