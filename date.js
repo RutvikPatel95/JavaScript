c4a2280f9aa7d7e06477fbdac9208781e88682b2
Mavo <rutvikpatel95862@gmail.com>
2023-07-17 15:42:50 +0530

    rutvik

---
+++ date.js
@@ -0,0 +1,58 @@
+/* javascript date objects*/
+
+/*
+1.new date();
+2.new date(date string)
+3.new date(year,month)
+4.new date(year,month,day)
+5.new date (year,month,day,hours)
+6.new date(year,month,day,hours,minutes)
+7.new date(year,month,day,hours,minutes,siconds)
+8.new date(milliseconds)
+*/
+1.
+{
+    // const dates=new Date()
+    // console.log(dates);
+}
+2.
+{
+    // let dates=new Date("2023-07-22")
+    // console.log(dates);
+}  
+3.
+{
+    // const dates=new Date(2023,3)
+    // console.log(dates);
+}
+
+4.
+{
+    // const dates=new Date(2003,3,15);
+    // console.log(dates);
+}
+5.
+{
+    // const dates=new Date(2023,14,16,16)
+    // console.log(dates);
+}
+6.
+{
+    // const dates=new Date(2023,14,16,16,45)
+    // console.log(dates);
+}    
+7.
+{
+    // const dates=new Date(2023,14,16,16,45,30)
+    // console.log(dates);
+}
+8.
+{
+    // const dates=new Date(2023,14,16,16,45,30,5)
+    // console.log(dates);
+}
+9.
+{
+    // const dates=new Date (200000000000);
+    // console.log(dates);
+}
---
+++ number.js
@@ -0,0 +1,66 @@
+//to string
+1.
+// const numbar=150;
+// const numbar1=numbar.toString();
+// const numbar2=(150).toString();
+// let numbar3=(120+30).toString()
+
+// console.log(numbar);
+// console.log(numbar1);
+// console.log(numbar2);
+// console.log(numbar3);
+
+// console.log(typeof(numbar));
+// console.log(typeof(numbar1));
+//     console.log(typeof(numbar2 ));
+//     console.log(typeof(numbar3));
+
+//  2.to exponenttial
+// var a=500000;
+// let b=a.toExponential();
+// let c=a.toExponential(2);
+// let d=a.toExponential(3);
+// let e=a.toExponential(4)
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+3.
+//to fixed
+
+// const a=6.500000;
+// let b=a.toFixed(1);
+// let c=a.toFixed(2);
+// let d=a.toFixed(3)
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+
+4.//to precision
+
+// const a=11.25698;
+// let b=a.toPrecision(2);
+// let c=a.toPrecision(3);
+// let d=a.toPrecision(4);
+// let e=a.toPrecision(5)
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+5.
+//valueof
+
+// const a=150;
+// let b=a.valueOf();
+
+// console.log(a);
+// console.log(b);
