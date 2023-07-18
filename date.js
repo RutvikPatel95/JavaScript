e1a13c81955d1e5354f26b0a19ce9b653318bfb5
Mavo <rutvikpatel95862@gmail.com>
2023-07-18 15:44:50 +0530

    date methode

--- date.js
+++ date.js
@@ -56,3 +56,114 @@
     // const dates=new Date (200000000000);
     // console.log(dates);
 }
+
+//javascript iso standard time
+
+{
+    // const dates=new Date(2023,3,16,20,20,25);
+    // console.log(dates.toString());
+    // console.log(dates.toDateString());
+    // console.log(dates.toUTCString);
+    // console.log(dates.toISOString()); 
+}
+
+/* ISO date , short date,long date*/
+{
+    // let dates=new Date("2023")
+    // console.log(dates);
+} 
+
+// short dates
+{
+    // let dates=new Date("03/12/2004")
+    // console.log(dates);
+}
+
+//long dates
+{
+    // let dates=new Date("march 23 2023")
+    // console.log(dates);
+
+    // let dates=new Date("23 march 2023")
+    // console.log(dates);
+}
+
+//date input-parsing dates
+{
+    // let msec = Date.Parse("march,21,2014");
+    // console.log(msec);
+
+}
+
+// javascript get date methode
+{
+    // let dates=new Date()
+    // console.log(dates);
+    // let year = dates.getFullYear();
+    // let month = dates.getMonth();
+    // let date =dates.getDate();
+    // let hours= dates.getHours();
+    // let minutes=dates.getMinutes();
+    // let second=dates.getSeconds();
+    // let milliseconds=dates.getMilliseconds();
+
+    // console.log(year);
+    // console.log(month);
+    // console.log(date);
+    // console.log(hours);
+    // console.log(minutes);
+    // console.log(second);
+    // console.log(milliseconds);
+
+}
+//javascript set Date methods
+1.
+{
+    // let dates =new Date();
+    // dates.setFullYear(2015)
+    // console.log(dates);
+}
+2.
+//month
+{
+    // let dates=new Date();
+    // dates.setMonth(7);
+    // console.log(dates);
+
+}
+3.
+//date
+{
+    // let dates=new Date()
+    // dates.setDate(31)
+    // console.log(dates);
+}
+4.
+//hours
+{
+    // let dates=new Date()
+    // dates.setHours(12)
+    // console.log(dates);
+}
+5.
+//minits
+{
+    // let dates=new Date()
+    // dates.setMinutes(35)
+    // console.log(dates);
+}
+6.
+//second
+{
+    // let dates=new Date()
+    // dates.setSeconds(48)
+    // console.log(dates);
+
+}
+7.
+ //milliseconds
+ {
+    // let dates=new Date()
+    // dates.setMilliseconds(2000)
+    // console.log(dates);
+ }
