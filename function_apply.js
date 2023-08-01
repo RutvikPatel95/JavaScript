ac7b49f1332308a2cdf6e3232f778754b7db5c69
Mavo <rutvikpatel95862@gmail.com>
2023-08-01 15:54:16 +0530

    rutvik

---
+++ function_apply.js
@@ -0,0 +1,49 @@
+//++++++++++++++++++++++++++++++++apply syntex++++++++++++++++++++++++++
+{
+    // name.apply(thisArg,argsArray)
+}
+//==============================apply()methode=========================
+{
+    //Object definition
+    // const name1={
+    //     school: "tapovan",
+    //     location: "Jackson"
+    // };
+
+    // function names(name , fees)
+    // {
+    //     return `${this.school},${name},${fees}`
+
+    // }
+     
+    // const result=names.apply(name1,["rutvik",65000])
+    // console.log(result);
+}
+//===========================apply()borrowing===============================
+{
+//     const name=
+//     {
+//         school:"tapovan",
+//         name1()
+//         {
+//             return `${this.school},"location is" ,${this.address}`
+//         }
+//     };
+//     let names={
+//         school:"tapovan",
+//         address:"surat",
+//     };
+//     let result=name.name1.apply(names);
+//    console.log(result); 
+}
+//==================================using two arrays()===============================
+{
+    // let name1=["jay","rutvik","vishal"];
+    // let name2=["prince","nil"];
+    // let name3=["mohit","nilesh","jatin"];
+
+    // let a=name1.push.apply(name1,name3,)
+    // console.log(name1);
+    // console.log(a);
+} 
+//============================using built-in function()==============================
